db3ecbaed86ca860b70b11f9c75c4d1ee0c5a771
Sohail Momin <sohail.momin1@gmail.com>
2025-07-05 17:25:16 +0200

    brute force solution

---
+++ product-of-array-except-self/solution1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def product_except_self(self, nums):
+        
+        res = []
+        for i in range(len(nums)):
+            prod = 1
+            for j in range(len(nums)):
+                if i != j:
+                    prod *= nums[j]
+            res.append(prod)
+        return res
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.product_except_self([1, 2, 3, 4]))  # Output: [24, 12, 8, 6]
